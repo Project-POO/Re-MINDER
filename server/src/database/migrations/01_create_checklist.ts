23ba0e5b6282a5f0cdaaa46b30b15655c7101e22
Barbosa <lucasbarbosa2807@gmail.com>
2020-09-06 11:22:28 -0300

    database

---
+++ server/src/database/migrations/01_create_checklist.ts
@@ -0,0 +1,17 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+    return knex.schema.createTable('checklist', table =>{
+        table.increments('id').primary();
+        table.string('title').notNullable();
+        table.timestamp('data')
+            .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+            .notNullable();
+        table.text('content').notNullable();
+        
+    });
+}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('checklist');
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,27 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 
-app.delete('/', (request, response) => {
-    console.log(request.params);
+//GET: Buscar ou listar uma informação
+//POST: Criar alguma nova informação
+//PUT:Atualizar uma informação exitente
+//DELETE: Deçetar uma informação existente
 
-    const users=[
-        { name: 'daniel', age: 22 },
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Paginação, Filtros, ordenação
+
+
+app.post('/users',(request, response) =>{
+    console.log(request.query)
+
+    const users= [
+        {name:'Diego', age : 25 },
+        {name: 'Vini', age:21},
     ];
 
-    return response.json(users)
+    return response.json(users);
 });
 
 app.listen(3333);
